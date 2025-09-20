eddc1d5068c575305d6c302e5cd5b367db6a275d
arantxa <149501497+arantxaMain@users.noreply.github.com>
2025-09-20 18:10:32 +0200

    feat: carga de fxml en clase app

--- src/main/java/es/arantxa/App.java
+++ src/main/java/es/arantxa/App.java
@@ -1,14 +1,26 @@
 package es.arantxa;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/gridpane.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
 
+        stage.setScene(scene);
+        stage.setTitle("FlowPane Alignment");
+        stage.setMinHeight(300);
+        stage.setMinWidth(500);
+        stage.setMaxHeight(500);
+        stage.setMaxWidth(800);
+        stage.show();
     }
 
     public static void main(String[] args) {
